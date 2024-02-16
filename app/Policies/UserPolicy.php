772d4b5cff949af34803996f8101c07c4fb17208
VoidGS <guilherme.garcia.s887@gmail.com>
2024-02-16 10:30:38 -0300

    chore: code style

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -5,29 +5,25 @@ namespace App\Policies;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class UserPolicy
-{
+class UserPolicy {
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): bool
-    {
+    public function viewAny(User $user): bool {
         //
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, User $model): bool
-    {
+    public function view(User $user, User $model): bool {
         //
     }
 
     /**
      * Determine whether the user can create models.
      */
-    public function create(User $user): bool
-    {
+    public function create(User $user): bool {
         // return $user->is_administrator;
         return $user->can('create users');
     }
@@ -35,32 +31,28 @@ class UserPolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, User $model): bool
-    {
+    public function update(User $user, User $model): bool {
         //
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, User $model): bool
-    {
+    public function delete(User $user, User $model): bool {
         //
     }
 
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, User $model): bool
-    {
+    public function restore(User $user, User $model): bool {
         //
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, User $model): bool
-    {
+    public function forceDelete(User $user, User $model): bool {
         //
     }
 }
